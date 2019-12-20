ab2cfb9a04a0eb95e78413ea373376771dfd7b5b
zulfiyebiser <zlfdnbsr@gmail.com>
2019-12-19 21:44:30 -0600

    added new class

--- src/test/java/com/vytrack/step_definitions/Hook.java
+++ src/test/java/com/vytrack/step_definitions/Hook.java
@@ -3,6 +3,8 @@ import com.vytrack.utilities.Driver;
 import io.cucumber.core.api.Scenario;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 public class Hook {
 
@@ -18,6 +20,8 @@ public class Hook {
         //if test failed - do this
         if(scenario.isFailed()){
             System.out.println("Test failed!");
+            byte[] screenshot = ((TakesScreenshot)Driver.get()).getScreenshotAs(OutputType.BYTES);
+            scenario.embed(screenshot,"image/png");
         }else{
             System.out.println("Cleanup!");
             System.out.println("Test completed!");
